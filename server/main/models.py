1a991ec5cd1cba822f0973d44ca189897415234d
DarcieNg <s3865443@rmit.edu.vn>
2023-12-19 10:32:01 +0700

    include template for models

---
+++ server/main/models.py
@@ -0,0 +1,28 @@
+"""
+    User:
+        - id
+        - name
+        - email
+        - password
+        - acc plan type (0, 1, 2, 3, etc. - default: 0 for trial)
+"""
+
+"""
+    Document - User:
+        - id
+        - user_id
+        - title
+        - content of original document
+        - content of processed document
+        - created_at
+        - updated_at (after modified timestamp)
+"""
+
+"""
+    Subscription packages:
+        - id
+        - name
+        - price
+        - period
+        - number of available documents (optional)
+"""
