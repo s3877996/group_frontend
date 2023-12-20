03a49214a49adcda5f964f56141966416f72ecc2
Duy Anh Pham <s3802674@student.rmit.edu.au>
2023-12-21 04:29:59 +0700

    Configure database

--- server/database/db.py
+++ server/database/db.py
@@ -1,1 +1,10 @@
 # Setting PostgreSQL database
+import os
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def init_db(app):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
