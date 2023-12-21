a52ef90ff20145007d4abbbd51ecc04a0d04cf3b
DarcieNg <s3865443@rmit.edu.vn>
2023-12-21 22:25:40 +0700

    Revert "add package"
    This reverts commit 63dd039d20243af4341cc7adf446241788735812.

--- server/config.py
+++ server/config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 import logging
 
 load_dotenv()
-SECRET_KEY =os.environ.get('SECRET_KEY')
+# SECRET_KEY =os.environ.get('SECRET_KEY')
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 # config.py
