659b032e03fd4fec96198af5bcb9b1388dd05960
ACER <s3681446@rmit.edu.vn>
2024-01-20 02:42:18 +0700

    Format Date, Edit Profile, Edit usermenu

--- client/src/pages/ProfilePage.js
+++ client/src/pages/ProfilePage.js
@@ -179,7 +179,7 @@ return (
                         className="w-full bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600 mt-4"
                         onClick={handleChangePassword}
                     >
-                        Change Password
+                        Update Account
                     </button>
                 </div>
             </div>
