f1e17f17eb54d03d32301721d853179dd8c465bb
s3877996 <s3877996@rmit.edu.vn>
2024-01-20 21:37:07 +0700

    Edit domain

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 // api.js
 import axios from 'axios';
 
-const baseURL = process.env.REACT_APP_API_BASE_URL || 'https://api.thptlethuy.online/?fbclid=IwAR1c1bhSa_qRg71PsqdldTprmdtr-M40K8BmekRkm8WSkxkzuqUaycdUAOw';
+const baseURL = process.env.REACT_APP_API_BASE_URL || 'https://api.prosepilot.site/?fbclid=IwAR3OqLnpHScTTTJ8Np_5wK1SV7H1JXmrijsRcKKvLyrKLWawJkVxJyN1MBk';
 
 const api = axios.create({
   baseURL,
